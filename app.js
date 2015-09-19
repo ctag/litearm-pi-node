7812af76644edc274d06c990b4716ed576f3818e
Christopher Bero <bigbero@gmail.com>
2015-09-19 13:17:20 -0500

    testing servo function

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+/**
+ * LiteArm-i2 controller
+ * Christopher Bero [csb0019@uah.edu]
+ */
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var childProcess = require('child_process');
+var spawn = childProcess.spawn;
+var fork = childProcess.fork;
+var exec = childProcess.exec;
+var StringDecoder = require('string_decoder').StringDecoder;
+var decoder = new StringDecoder('utf8');
+var fs = require('fs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,8 +34,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//app.use('/', routes);
+//app.use('/users', users);
+
+function setServo(servo, percent)
+{
+  if (servo < 1 || servo > 3) {
+    return;
+  } else if (percent < 0 || percent > 100) {
+    return;
+  }
+  exec('echo '+servo+'='+percent+'% > /dev/servoblaster',
+  function (error, stdout, stderr) {
+    console.log("set servo: ", servo, " -> ", percent);
+  });
+}
+
+setServo(1, 50);
+setServo(2, 50);
+setServo(3, 50);
+
+app.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/cmd', function(req, res, next) {
+  //
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
